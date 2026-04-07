dea80731c603249b197795515ee0429c220b0554
unknown <sebaslpez0519@gmail.com>
2026-04-06 20:03:54 -0500

    Se agrega estructura hexagonal base

---
+++ applications/app-service/src/test/java/com/b2chat/order_manager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2chat.order_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class OrderManagerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
